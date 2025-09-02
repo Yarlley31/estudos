5a1a26eb1a03c5bbe5dfd72ff429ad0d07a06bda
Yarlley Fernandes <147113204+Yarlley31@users.noreply.github.com>
2025-09-02 14:51:17 -0300

    pilha

---
+++ fifo/code1.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct 
+{
+    int month, day, year;
+} Date;
+
+typedef struct{
+    char name[50];
+    Date birth;
+} Person;
+
+typedef struct Node
+{
+    Person p;
+    struct Node *next;
+} Node;
+
+Person create_person(){
+    Person p;
+
+    printf("Digite o nome da pessoa e data de nascimento dd mm aaaa: ");
+    scanf("%s %d %d %d", p.name, p.birth.day, p.birth.month, p.birth.year);
+    return p;
+}
+int main(){
+
+
+}
