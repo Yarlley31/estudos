75f9e83d09b2b4bc9636902dc341e175fa7a5c92
Yarlley Fernandes <147113204+Yarlley31@users.noreply.github.com>
2025-09-03 17:45:32 -0300

    update

---
+++ fifo/aula.c
@@ -0,0 +1,93 @@
+﻿/*
+teria que ver o ultimo adicionado e entao removelo?
+*/
+
+/*
+v = [ , , , , , ]
+n = quantidade de elementos do vetor
+vetor[n++] = value;
+agora n = 1;
+vetor[n++] = value;
+n = 2;
+
+vetor[n--]; // remover
+n = 1;
+
+último elemento 
+stack[n-1];
+*/
+
+
+/*
+
+fila com vetor com lista circular
+
+dois indices 
+i = inicio
+j = fim
+m = total de possiveis elementos
+
+i++ % m (nao entendi direito) 
+
+*/
+
+/*
+
+fila de prioridade
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+    int value;
+    struct Node* next;
+} Node;
+
+typedef struct stack
+{
+    Node* top;
+    int size;
+} Stack;
+
+Node* create(n){
+    Node *new = (Node*) malloc(sizeof(Node));
+
+    if(new == NULL){
+        error();
+    }
+
+    new->value = n;
+    new->next = NULL;
+
+    return new;
+}
+
+void push_first(Node** head, Node** tail, int n){
+    Node *new = create(n);
+
+    *head = new;
+    *tail = new;
+    new->next = NULL;
+}
+void push(Node** tail,Node** aux_tail, int n){
+    Node *new = create(n);
+
+    *tail = aux_tail;
+    (*aux_tail)->next = new;
+    *aux_tail = new;
+    new->next = NULL;
+}
+
+void delete(Node** tail){}
+int main(){
+    Node* head = NULL;
+    Node* tail = NULL;
+    Node* aux_tail = NULL;
+
+    push_first(&head, &tail, 10);
+    push(&tail, &aux_tail, 15);
+    push(&tail, &aux_tail, 20);
+}
---
+++ fifo/priority_queue.c
@@ -0,0 +1,98 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int value;
+    int priority; // 1 maior prioridade, 2 menos, 3 menos, ...
+    struct node* next;
+} Node;
+
+Node* create_node(int value, int priority){
+    Node* new_node = (Node*)malloc(sizeof(Node));
+    if (new_node == NULL){
+        printf("Erro de alomanento.\n");
+        EXIT_FAILURE;
+    }
+
+    new_node->value = value;
+    new_node->priority = priority;
+    new_node->next = NULL;
+    return new_node;
+}
+
+void priority_insertion(Node** head, int value, int priority){
+    Node* new_node = create_node(value, priority);
+
+    // caso 1: lista vazia ou a prioridade do novo elemento é maior que a prioridade da cabeça
+    if (*head == NULL || priority < (*head)->priority){ // se lista varia ou prioridade do novo elemento é menor do que prioridade do primeiro elemento
+        new_node->next = *head;
+        *head = new_node;
+    } else {
+        // caso 2: procurar o local certo pra inserir o elemento
+        Node* aux = *head;
+        while (aux->next != NULL && priority >= aux->next->priority){ // enquanto aux next diferente de NULL e prioridade do novo elemento é maior doq a prioridade do aux->next
+            aux = aux->next;
+        }
+        new_node->next = aux->next;
+        aux->next = new_node;
+    }
+}
+
+void print(Node* head){
+    if (head == NULL){
+        printf("A fila está vazia.\n");
+    } else {
+        Node* aux = head;
+        while (aux != NULL)
+        {
+            printf("P%d, %d, -> ", aux->priority, aux->value);
+            aux = aux->next;
+        }
+        printf("NULL\n");
+    }
+
+}
+
+void remove_higher_priority(Node** head){
+    if (*head == NULL){
+        printf("A fila já está vazia.\n");
+    }
+
+    Node* to_be_removed = *head;
+    *head = (*head)->next;
+    free(to_be_removed);
+}
+
+void free_total(Node** head){
+    if (*head == NULL){
+        printf("A fila já está vazia.\n");
+        return;
+    }
+    Node* aux;
+    while (*head != NULL) {
+        aux = *head;
+        *head = aux->next;
+        free(aux);
+    }
+}
+
+int main(){
+    Node* queue = NULL;
+    priority_insertion(&queue, 200, 3);
+    priority_insertion(&queue, 100, 2);
+    priority_insertion(&queue, 250, 4);
+    priority_insertion(&queue, 500, 4); // ta sobrescrevendo quando de mesma prioridade;
+    priority_insertion(&queue, 2, 1);
+
+    print(queue);
+    remove_higher_priority(&queue);
+    print(queue);
+
+    priority_insertion(&queue, 50, 1);
+    print(queue);
+
+    free_total(&queue);
+
+    return 0;
+}
